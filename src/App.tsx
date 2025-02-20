a4e150ba2447ed3552cf141f26832c3ab2a3dbf6
Hasan  Elmaghraby <maghrabyhasan@gmail.com>
2025-02-20 20:24:00 +0200

    create pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Offers from "./pages/OFFers";
+import ForgotPassword from "./pages/ForgotPassword";
+
 function App() {
-  return <div className="m-5">guylkhj</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="*" element={<div>404</div>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="sign-in" element={<SignIn />} />
+          <Route path="sign-up" element={<SignUp />} />
+          <Route path="forgot-password" element={<ForgotPassword />} />
+          <Route path="offers" element={<Offers />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
