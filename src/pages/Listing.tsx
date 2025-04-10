1764a059a7ee0ed7b2fa26000bc2bf3f2eded3b0
Hasan  Elmaghraby <maghrabyhasan@gmail.com>
2025-04-10 18:06:26 +0200

    add share functionality

--- src/pages/Listing.tsx
+++ src/pages/Listing.tsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Spinner } from "../components/shared/Spinner";
-
+import { FaShare } from "react-icons/fa";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, Navigation, EffectFade } from "swiper/modules";
 import "swiper/css";
@@ -15,7 +15,7 @@ function Listing() {
 
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
-
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
   useEffect(() => {
     if (!id) return;
 
@@ -60,6 +60,23 @@ function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div
+        className="fixed top-[10%] right-[3%] bg-white cursor-pointer w-12 h-12 flex justify-center items-center text-sm p-1 z-10 border border-gray-400 rounded-full "
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <FaShare className="text-lg text-slate-500" />
+      </div>
+      {shareLinkCopied && (
+        <p className="fixed top-[15%] right-[6%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">
+          Link Copied
+        </p>
+      )}
     </main>
   );
 }
